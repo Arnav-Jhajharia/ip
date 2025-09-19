f6951f982097c8a5bf5766579a7191a519c84f5b
arnav-jhajharia-2 <e1486333@comp.nus.edu.sg>
2025-09-19 18:00:09 +0800

    Made GUI better

--- src/main/java/com/arnavjhajharia/penguin/app/Launcher.java
+++ src/main/java/com/arnavjhajharia/penguin/app/Launcher.java
@@ -1,4 +1,8 @@
+// src/main/java/com/arnavjhajharia/penguin/app/Launcher.java
 package com.arnavjhajharia.penguin.app;
 
 public class Launcher {
+    public static void main(String[] args) {
+        MainFx.main(args);
+    }
 }
